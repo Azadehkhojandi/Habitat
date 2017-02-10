f9ce62e0dde9e132d1a1ad3092f9616ec72b6eb6
Azadeh Khojandi <azadehkhojandi@gmail.com>
2017-02-10 23:56:39 +1100

    Az config changes

--- gulp-config.js
+++ gulp-config.js
@@ -1,5 +1,5 @@
 module.exports = function () {
-  var instanceRoot = "C:\\websites\\Habitat.dev.local";
+    var instanceRoot = "C:\\lab\\habitat82rev161115";
   var config = {
     websiteRoot: instanceRoot + "\\Website",
     sitecoreLibraries: instanceRoot + "\\Website\\bin",
